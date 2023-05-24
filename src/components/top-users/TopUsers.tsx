ea6b091fc08bcfa71b877a9c9ad104c37c9524d8
Youcef <youcef.ettaieb@gmail.com>
2023-05-24 10:28:04 +0200

    Test vercel build

--- src/components/top-users/TopUsers.tsx
+++ src/components/top-users/TopUsers.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   flex: 1;
-  background-color: #51e2f5;
+  background-color: red;
   border-radius: 5px;
   margin-right: 10px;
 `;
